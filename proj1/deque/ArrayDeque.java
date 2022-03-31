2fdd0f7a88d479fc59d5914a513aef3f65625e68
wangzihao <cau_wangzihao@163.com>
2022-03-31 17:01:58 +0800

    checkpoint
    finish the basis for the check point

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,166 @@
+package deque;
+public class ArrayDeque<T> {
+    private int size;
+    private T[] items;
+    public int nextFirst;
+    public int nextLast;
+
+
+
+    // ALL HELPER METHOD !!!!
+
+    /**
+     * check if the items variable need to be resized larger
+     * otherwise we can not update the nextFirst or nextLast
+     * value in this current array
+     */
+    private boolean NeedLarger() {
+        if (nextFirst == 0 && nextLast == items.length - 1) {
+            return true;
+        }
+        if (nextFirst > nextLast && nextFirst - nextLast == 1){
+            return true;
+        }
+        return false;
+    }
+
+    private int getFrontIndex(){
+        if (nextFirst == items.length-1){
+            return 0;
+        }
+        return nextFirst-1;
+    }
+
+    private int getBackIndex(){
+        if (nextLast == 0){
+            return items.length-1;
+        }
+        return nextLast-1;
+    }
+
+    private void resize(int capacity){
+        T[] tempt = (T[]) new Object[capacity];
+        int front = getFrontIndex();
+        int back = getBackIndex();
+        int current_length = items.length;
+        if (front < back){
+            System.arraycopy(items,front,tempt,0,size);
+            items = tempt;
+            nextFirst = items.length-1;
+            nextLast = size;
+            return;
+        }
+        System.arraycopy(items,front,tempt,0,current_length-front);
+        System.arraycopy(items,0,tempt,current_length-front,back+1);
+        items = tempt;
+        nextFirst = items.length-1;
+        nextLast = size;
+        return;
+    }
+
+    /**update nextLast variable after we call addLast method
+     * for the object whose items need NOT Enlarger !!!!
+     */
+
+    private void updateNextLast(){
+        if (nextLast == items.length-1){
+            nextLast = 0;
+        }else {
+            nextLast += 1;
+        }
+    }
+
+    private void updateNextFirst(){
+        if (nextFirst == 0){
+            nextFirst = items.length - 1;
+        }else{
+            nextFirst -= 1;
+        }
+    }
+
+
+    // START TO IMPLEMENT!!!!
+
+    // create a empty arraydeque.
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+        nextLast = 5;
+        nextFirst = 4;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        if (size() == 0){
+            return true;
+        }
+        return false;
+    }
+
+
+    public void addFirst(T item){
+        if (NeedLarger()){
+            resize(2* items.length);
+        }
+        items[nextFirst] = item;
+        updateNextFirst();
+        size += 1;
+    }
+
+    public void addLast(T item){
+        if (NeedLarger()){
+            resize(2* items.length);
+        }
+        items[nextLast] = item;
+        updateNextLast();
+        size += 1;
+    }
+
+    public T removeFirst(){
+        if (isEmpty()){
+            return null;
+        }
+        int front = getFrontIndex();
+        T x = items[front];
+        items[front] = null;
+        nextFirst = front;
+        size -= 1;
+        return x;
+    }
+
+    public T removeLast(){
+        if (isEmpty()){
+            return null;
+        }
+        int back = getBackIndex();
+        T x = items[back];
+        items[back] = null;
+        nextLast = back;
+        size -= 1;
+        return x;
+    }
+
+    public T get(int index){
+        if (isEmpty()){
+            return null;
+        }
+        int front = getFrontIndex();
+        int back = getBackIndex();
+        if (index > size-1 | index < -1){
+            return null;
+        }
+        if (front < back){
+            return items[front+index];
+        }
+        if (index < items.length-front){
+            return items[front+index];
+        }
+        return items[index-(items.length-front)];
+    }
+
+
+
+}
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -2,13 +2,13 @@ package deque;
 
 public class LinkedListDeque<T> {
 
-    private class Dnode{
+    private  class Dnode {
         public T item;
         public Dnode prev;
         public Dnode next;
 
         //constructor of Dnode
-        public Dnode(Dnode p, T i, Dnode n){
+        public Dnode(Dnode p, T i, Dnode n) {
             prev = p;
             item = i;
             next = n;
@@ -19,43 +19,43 @@ public class LinkedListDeque<T> {
     private Dnode sentinel;
 
     // create empty Deque
-    public LinkedListDeque(){
-        sentinel = new Dnode(null,null,null);
+    public LinkedListDeque() {
+        sentinel = new Dnode(null, null, null);
         sentinel.prev = sentinel;
         sentinel.next = sentinel;
         size = 0;
     }
 
-    public void addFirst(T x){
+    public void addFirst(T x) {
         size += 1;
         sentinel.next.prev = new Dnode(sentinel, x, sentinel.next);
         sentinel.next = sentinel.next.prev;
     }
 
     // add a element in the last of list.
-    public void addLast(T x){
+    public void addLast(T x) {
         size += 1;
         sentinel.prev.next = new Dnode(sentinel.prev, x, sentinel);
         sentinel.prev = sentinel.prev.next;
     }
 
     // return whether a list is empty
-    public boolean isEmpty(){
-        if (sentinel.next == sentinel){
+    public boolean isEmpty() {
+        if (sentinel.next == sentinel) {
             return true;
         }
         return false;
     }
 
     //return the size of the list.
-    public int size(){
+    public int size() {
         return size;
     }
 
     // remove and return the first item from the list.
-    public T removeFirst(){
+    public T removeFirst() {
         // handle for the base case: empty list
-        if (this.isEmpty()){
+        if (this.isEmpty()) {
             return null;
         }
         // handle for the none trivial case, this time do not consider empty situation.
@@ -67,9 +67,9 @@ public class LinkedListDeque<T> {
     }
 
     //remove and return the last item from the list.
-    public T removeLast(){
+    public T removeLast() {
         // handle the base case.
-        if (this.isEmpty()){
+        if (this.isEmpty()) {
             return null;
         }
         // handle for the none trivial case, this time do not consider the empty situation.
@@ -79,6 +79,44 @@ public class LinkedListDeque<T> {
         sentinel.prev.next = sentinel;
         return target;
     }
-    
 
+    public T get(int index) {
+        if (isEmpty() == true) {
+            return null;
+        }
+        if (index > size() - 1) {
+            return null;
+        }
+        Dnode actualDeque = sentinel;
+        for (int i = 0; i <= index; i += 1) {
+            actualDeque = actualDeque.next;
+        }
+        return actualDeque.item;
+    }
+
+    public void printDeque() {
+        for (int i = 0; i < size(); i += 1) {
+            System.out.print(get(i) + " ");
+        }
+        System.out.println();
+    }
+
+
+    public T getRecursive(int index) {
+        if (isEmpty()) {
+            return null;
+        }
+        if (index >= size() | index < 0) {
+            return null;
+        }
+        if (index == 0){
+            return sentinel.next.item;
+        }
+        LinkedListDeque<T> tempt = new LinkedListDeque<>();
+        tempt.sentinel.next = sentinel.next.next;
+        tempt.size = size() - 1;
+        return tempt.getRecursive(index-1);
+    }
+
+    
 }
